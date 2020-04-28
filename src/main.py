3bde3ba9c9b330787d5de97204fa228044b69ac0
Vincent Troy Allen Jr <vtad4f@mst.edu>
2020-04-27 22:48:27 -0500

    Fiddling with a src file

---
+++ src/main.py
@@ -0,0 +1,36 @@
+
+from scipy.spatial import distance
+
+# distance.euclidean([], [])
+
+
+class Cluster(object):
+   """
+      BRIEF  
+   """
+   
+   def __init__(self, node):
+      """
+         BRIEF  
+      """
+      self.nodes = [node]
+      
+      
+class Clusters(object):
+   """
+      BRIEF  
+   """
+   
+   def __init__(self):
+      """
+         BRIEF  
+      """
+      self.clusters = []
+      
+      
+   def GetNextNode(n):
+      """
+         BRIEF  
+      """
+      
+      
