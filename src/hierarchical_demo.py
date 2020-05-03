2439a5cc0678f6a96d0d1c0f980452393bb31347
Vincent Troy Allen Jr <vtad4f@mst.edu>
2020-05-03 16:32:20 -0500

    Added dendrogram

--- src/hierarchical_demo.py
+++ src/hierarchical_demo.py
@@ -2,6 +2,7 @@
 
 from matplotlib.colors import XKCD_COLORS
 from matplotlib import pyplot as graph
+from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.datasets import make_blobs
 from sklearn.metrics import accuracy_score
@@ -10,16 +11,16 @@ from sklearn.metrics import accuracy_score
 COLORS = list(XKCD_COLORS.values()) # just a big list of colors
 
 
-def Run(X, y, mdl):
+def RunSklearn(X, y, mdl):
     """
         BRIEF  Show the graph before and after clustering
     """
     for i in range(len(X)):
         graph.scatter(X[i,0], X[i,1], c=COLORS[y[i]])
     graph.show()
-
+    
     y_clustered = mdl.fit_predict(X)
-
+    
     for i in range(len(X)):
         graph.scatter(X[i,0], X[i,1], c=COLORS[y_clustered[i]])
     graph.show()
@@ -29,22 +30,27 @@ if __name__ == '__main__':
     """
         BRIEF  Main execution
     """
-    proceed = True
-    while proceed:
-    
-        option = input("\nHow would you like to cluster?\n1. Distance threshold\n2. Hard-coded number of clusters\n")
+    while True:
+        
+        option = input("\nHow would you like to cluster?\n 1. sklearn distance threshold\n 2. sklearn n clusters\n 3. scipy dendrogram\n 4. exit\n")
         
         if option == '1':
             scale = 3
-            centers = [1,1], [9*scale,9*scale]
-            X, y = make_blobs(n_samples = 200*scale, centers = centers, cluster_std = 2*scale)
+            X, y = make_blobs(n_samples = 200*scale, centers = [[1,1], [9*scale,9*scale]], cluster_std = 2*scale)
             mdl = AgglomerativeClustering(n_clusters = None, distance_threshold = 40*scale)
-            Run(X, y, mdl)
+            RunSklearn(X, y, mdl)
             
         elif option == '2':
-            centers = 2
-            X, y = make_blobs(n_samples = 200, centers = centers, cluster_std = 2)
+            X, y = make_blobs(n_samples = 200, centers = 2, cluster_std = 2)
             mdl = AgglomerativeClustering(n_clusters = centers)
-            Run(X, y, mdl)
+            RunSklearn(X, y, mdl)
+            
+        elif option == '3':
+            X, y = make_blobs(n_samples = 200, centers = 2, cluster_std = 2)
+            d = dendrogram(linkage(X, method='ward'))
+            graph.show()
+            
+        elif option == '4':
+            break
             
             
