4579250e50cfc5cc52c599f1125b3c853830aceb
Vincent Troy Allen Jr <vtad4f@mst.edu>
2020-05-06 18:05:26 -0500

    Saving data with bad formatting
    before using python to correct it

---
+++ src/json_to_csv.py
@@ -0,0 +1,35 @@
+
+
+from collections import OrderedDict
+import csv
+import json
+import os
+
+
+DATA_DIR = 'data'
+
+
+if __name__ == '__main__':
+    """
+        BRIEF  Parse https://db.ygoprodeck.com/api/v7/cardinfo.php
+               and translate it to a csv file so that the multiflow
+               FileStream can read it.
+    """
+    # read everything
+    with open(os.path.join(DATA_DIR, 'all.json'), 'r') as f:
+        content = json.loads(f.read(), object_pairs_hook=OrderedDict)['data']
+        
+    header_row = content[0].keys()
+    
+    # write everything
+    with open(os.path.join(DATA_DIR, 'all.csv'), 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header_row)
+        for row in content:
+            try:
+                writer.writerow(row.values())
+            except UnicodeEncodeError:
+                print(i)
+                
+    # write for 
+    
