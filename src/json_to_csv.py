4362600fbd5e7360666db05750ffbb797a9d1577
Vincent Troy Allen Jr <vtad4f@mst.edu>
2020-05-06 18:41:50 -0500

    Error msg tweak

--- src/json_to_csv.py
+++ src/json_to_csv.py
@@ -16,20 +16,19 @@ if __name__ == '__main__':
                FileStream can read it.
     """
     # read everything
-    with open(os.path.join(DATA_DIR, 'all.json'), 'r') as f:
+    with open(os.path.join(DATA_DIR, 'prices.json'), 'r') as f:
         content = json.loads(f.read(), object_pairs_hook=OrderedDict)['data']
         
     header_row = content[0].keys()
     
     # write everything
-    with open(os.path.join(DATA_DIR, 'all.csv'), 'w', newline='') as f:
+    with open(os.path.join(DATA_DIR, 'prices.csv'), 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header_row)
         for row in content:
             try:
                 writer.writerow(row.values())
             except UnicodeEncodeError:
-                print(i)
+                print("Invalid row! ID {0}".format(row["id"]))
                 
-    # write for 
-    
+                
